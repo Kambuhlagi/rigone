84c33de623d05c639510958907359ade181d9ece
Kambuhlagi <96752054+Kambuhlagi@users.noreply.github.com>
2022-04-08 13:44:54 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget https://github.com/xmrig/xmrig/releases/download/v6.15.1/xmrig-6.15.1-msvc-win64.zip
-xmrig-6.15.1-msvc-win64.zip
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
+xmrig-6.17.0-linux-x64.tar.gz
 ./xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u XMR:89vETEeFsvbLmtHqyJTkyNa1zvdiUsSs61Cm9ssHQBjcf8RA4bQKMUU6fHDYGxkzEjRJkzJ3qiCauLbHigUMLaxERL49cVJ.QEWE_NAME -p x
